9ef9e05f1fd9e6abfdda73b8409e86e604d78857
DuyTrung84 <115273824+DuyTrung84@users.noreply.github.com>
2022-11-22 22:29:46 +0700

    Add files via upload

---
+++ user/inc/footer.php
@@ -0,0 +1,35 @@
+<footer>
+            <table >
+                <tr>
+                    <td><b>Về chúng tôi</b></td>
+                    <td></td>
+                    <td><b>Chính sách</b></td>
+                    <td><b>Liên hệ</b></td>
+                </tr>
+                <tr>
+                    <td>Wiki</td>
+                    <td>Việc làm</td>
+                    <td>Chính sách bảo mật</td>
+                    <td>Hotline: 19001000</td>
+                    
+                </tr>
+                <tr>
+                    <td>Giới thiệu</td>
+                    <td>Góp ý</td>
+                    <td>Quy định sử dụng</td>
+                    <td></td>
+                    
+                </tr>
+                <tr>
+                    <td>Quên mật khẩu</td>
+                    <td>Đăng ký thành viên</td>
+                    <td>Giải quyết khiếu nại</td>
+                    <td></td>
+                    
+                </tr>
+            </table>
+        </footer>
+    </div>
+
+</body>
+</html>
---
+++ user/taikhoan.php
@@ -0,0 +1,26 @@
+<?php 
+    include './inc/header.php';
+?>
+
+        <div class="background">
+            <main>
+
+                <aside>
+                    <div class="avt">
+                        <img src="./img/avt.png" alt="">
+                        <input type="submit" name="" id="" value="Chỉnh sửa thông tin tài khoản">
+                    </div>
+                </aside>
+                <article>
+                    <div class="information">
+                        <h3>Thông tin tài khoản</h3><hr>
+                        <p>Email: <span></span></p>
+                        <p>Họ tên: <span></span></p>
+                        <p>SDT: <span></span></p>
+                    </div>
+                </article>
+            </main>
+        </div><br><br>
+<?php 
+    include './inc/footer.php';
+?>
